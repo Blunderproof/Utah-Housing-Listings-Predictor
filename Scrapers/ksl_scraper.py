053bb06bbac9db963195a688afe06f19d317b9be
Eric Fortney <eafortney2@gmail.com>
2019-03-28 19:08:25 -0600

    updated scraper to be more robust, have starting csv set

--- Scrapers/ksl_scraper.py
+++ Scrapers/ksl_scraper.py
@@ -4,6 +4,7 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import json
+import csv
 
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
@@ -11,8 +12,19 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
 
-# Opens an instance of Chrome to enable the JS scroll update, adding more listings to the search, then collect their URIs 
-def seleniumScrape(pagedown_count=2):
+def addUniqueListingIDsToQueue(listingIDs, fileSrc):
+    # get the current queue of listings
+    with open(fileSrc, 'r') as f:
+        currQueue = [listing.strip('\n') for listing in f.readlines()]
+    uniqueListings = list( set(currQueue) - set(listingIDs))
+
+    with open(fileSrc, 'a') as f:
+        for listing in uniqueListings:
+            f.write("%s\n" % listing)
+    print("Finished adding %s unique listings to the queue", len(uniqueListings))
+
+# Opens an instance of Chrome to enable the JS scroll update, adding more listings to the search, then it collect their URIs 
+def seleniumScrape(pagedown_count=200):
     listingIDs = []
 
     browser = webdriver.Chrome(ChromeDriverManager().install())
@@ -30,15 +42,19 @@ def seleniumScrape(pagedown_count=2):
 
     # Find all listing after scrolling down and loading more content 
     listingObjs = browser.find_elements_by_class_name("Listing")
-    for listing in listingObjs:
-        listingIDs.append(listing.get_attribute("id"))
-    return listingIDs
 
+    listingIDs = [listing.get_attribute("id") for listing in listingObjs]
+    # save the Listing IDs to a file
+    with open('listingIDs.txt', 'w') as f:
+        for listingID in listingIDs:
+            f.write("%s\n" % listingID)
+    print("Finished collecting listings")
+
+    return listingIDs
 
 def extractListingDetails(base_url, listingID):
     # url = "https://homes.ksl.com/listing/40310982"
     # Need to send a proper user agent header so that we don't get a 403 Forbidden response
-    print("Extracting from Listing: ", listingID)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
     }
@@ -57,14 +73,88 @@ def extractListingDetails(base_url, listingID):
     }
     return listingDict
 
-def saveToLog(listingIDs):
-    listings = []
-    for listingID in listingIDs:
-        listings.append(extractListingDetails("https://homes.ksl.com/listing/", listingID))
-    with open('listings.json', 'w+') as outfile:  
-        json.dump(listings, outfile, indent=4)
-    print("Saved listings to log")
+# save all currently collected listings to a json
+def collect(listingIDs):
+    # the used listings list is for preventing duplicate searching of a given listing if an exception is encountered
+    listingData = []
+    usedListingIDs = []
+
+    # if an error is encountered save what has been collected, try again
+    try:
+        for i, listingID in enumerate(listingIDs):
+            print("Extracting from Listing: %s, %s of %s" % (listingID, i, len(listingIDs)))
+            
+            usedListingIDs.append(listingID)
+            listingData.append(extractListingDetails("https://homes.ksl.com/listing/", listingID))
+
+        # Entire queue processed at this point
+        # add the listing data to a csv
+        print("FINISH PROCESSING QUEUE ----------------")
+        keys = listingData[0]['content'].keys()
+        with open('listings.csv', 'a', newline='') as outfile:
+            dict_writer = csv.DictWriter(outfile, fieldnames=keys)
+            dict_writer.writerows(listingData)
+        print("Saved listings to log")
+
+        # Remove all listings from queue file
+        open('listingIDs.txt', 'w').close()
+
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt, saving data to csv")
+
+        # Add the listing data to a csv
+        keys = listingData[0].keys()
+        with open('listings.csv', 'a', newline='') as outfile:
+            dict_writer = csv.DictWriter(outfile, fieldnames=keys)
+            dict_writer.writerows(listingData)
+        
+        # update the queue
+        unusedListings = list( set(listingIDs) - set(usedListingIDs))
+        with open("listingsIDs.txt", "w") as f:
+            for listing in unusedListings:
+                f.write("%s\n" % listing)
+
+        
+    except Exception as e:
+        print("Exception found: ----------------------------------")
+        print(e)
+        print("\nSaving what has been currently found and continuing listing queue")
+        
+        # Add the listing data to a csv
+        keys = listingData[0].keys()
+        with open('listings.csv', 'a', newline='') as outfile:
+            dict_writer = csv.DictWriter(outfile, fieldnames=keys)
+            dict_writer.writerows(listingData)
+
+        # update the queue
+        unusedListings = list( set(listingIDs) - set(usedListingIDs))
+        with open("listingsIDs.txt", "w") as f:
+            for listing in unusedListings:
+                f.write("%s\n" % listing)
+
+        # re-run the collect function on the remaining listings
+        collect(unusedListings)
+
 
 if __name__ == "__main__":
-    listingIDs = seleniumScrape()
-    saveToLog(listingIDs)
+    # scrape IDs
+    # listingIDs = seleniumScrape()
+    # addUniqueListingIDsToQueue(listingIDs, fileSrc="listingIDs.txt")
+
+    # grab the queue
+    with open('listingIDs.txt', 'r') as f:
+        listingIDs = [listing.strip('\n') for listing in f.readlines()]
+        print("Recovering Listing IDs from file")
+    # process the queue
+
+    # Init the CSV
+    print("Initializing the CSV")
+    sampleListing = extractListingDetails("https://homes.ksl.com/listing/", listingIDs[0])
+    keys = sampleListing['content'].keys()
+    # print(sampleListing)
+    print(keys)
+    with open('listings.csv', 'w', newline='') as outfile:
+        dict_writer = csv.DictWriter(outfile, fieldnames=keys)
+        dict_writer.writeheader()
+
+    collect(listingIDs)
